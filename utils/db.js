719cbaf6e561e4a5bbfdaa3e81bdfc001349a646
damadet <adedokundamilare41@gmail.com>
2023-10-03 19:47:55 +0100

    db

--- utils/db.js
+++ utils/db.js
@@ -4,10 +4,10 @@ const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = 'mongodb+srv://admin:Artist582@cluster0.a6ecjrz.mongodb.net/?retryWrites=true&w=majority';
-// const dbUrl = `mongodb://${HOST}:${PORT}`;
+const dbUrl = `mongodb://${HOST}:${PORT}`;
 class DBClient {
   constructor() {
-    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client = new MongoClient(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true });
     this.client.connect().then(() => {
       this.db = this.client.db(`${DATABASE}`);
     }).catch((err) => {
